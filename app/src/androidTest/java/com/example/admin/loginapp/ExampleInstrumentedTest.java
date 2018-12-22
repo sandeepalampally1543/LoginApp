486f96adc57e0a15cef55e6c58f5d341161fc629
sandeepalampally1543 <33712608+sandeepalampally1543@users.noreply.github.com>
2018-12-22 12:11:02 +0530

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/admin/loginapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/admin/loginapp/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.admin.loginapp", appContext.getPackageName())
+        assertEquals("com.example.admin.loginapp", appContext.getPackageName());
     }
 }
